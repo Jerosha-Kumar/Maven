369df190655d26315923d8ef3717a0bdc06b100b
Jerosha Kumar <jeroshakumar.27csa@licet.ac.in>
2026-03-10 10:49:17 +0530

    initial commi

---
+++ src/main/java/com/example/demo/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/register")
+    public String showForm() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String password,
+            Model model) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+
+        return "success";
+    }
+}
